6b363e95df06b22c453b7521a4a67b6826542bb7
Mohan Sai <113694256+Chowdary-Hacker@users.noreply.github.com>
2024-04-24 18:31:18 +0530

    Add files via upload

---
+++ backend/routers/studentInfo.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const studentInfoModel = require("../models/studentInfo");
+
+const router = express.Router();
+
+router.post("/",(req,res)=>{
+    let {name,email,age} = req.body;
+    let student =  studentModel({name,email,age});
+    student.save();// insert the document the database
+    res.json(student);
+});
+
+router.get("/",async (req,res)=>{
+    let studentsList = await  studentModel.find();
+    res.json(studentsList);
+});
+
+router.put("/:id",async (req,res)=>{
+    let {name,email,age} = req.body;
+    let id = req.params.id;
+    let student = await  studentModel.findByIdAndUpdate(id,{name,age,email},{new:false});
+    res.json(student);
+});
+
+router.delete("/:id",async (req,res)=>{
+    let s = await  studentModel.findByIdAndDelete(req.params.id);
+    if(s != null) res.json(s);
+    else res.status(404).send("No such student found");
+})
+
+module.exports = router
